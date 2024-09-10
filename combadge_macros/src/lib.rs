eb3ca323857eb61edc247c9605d30d9eeed02d61
Dan Stoza <stoza@periapt.media>
2024-09-10 14:24:15 -0700

    Create basic client and server types

--- combadge_macros/src/lib.rs
+++ combadge_macros/src/lib.rs
@@ -1,9 +1,71 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
+use quote::{format_ident, quote};
+use syn::{parse, parse_macro_input, FnArg, ItemTrait, TraitItem};
+
 #[proc_macro_attribute]
 pub fn combadge(attr: TokenStream, item: TokenStream) -> TokenStream {
-    println!("attr: {attr}");
-    println!("item: {item}");
-    item
+    let mut item: ItemTrait = parse_macro_input!(item);
+
+    let Some(TraitItem::Fn(constructor)) = item.items.iter().find(|item| match item {
+        TraitItem::Fn(function) => function.sig.ident == "new",
+        _ => false,
+    }) else {
+        panic!("Failed to find new function in {}", item.ident);
+    };
+
+    let constructor_args = &constructor.sig.inputs;
+
+    if constructor_args
+        .iter()
+        .any(|arg| matches!(arg, FnArg::Receiver(_)))
+    {
+        panic!("Expected new function to not have a self argument");
+    };
+
+    println!("Found constructor args {constructor_args:?}");
+
+    let constructor_args = constructor_args.iter();
+
+    let client_name = format_ident!("{}Client", item.ident);
+    let client = quote! {
+        #[derive(Debug)]
+        pub struct #client_name {
+            client: std::rc::Rc<std::cell::RefCell<::combadge::prelude::Client>>,
+        }
+
+        impl #client_name {
+            pub fn new(worker: ::combadge::prelude::web_sys::Worker #(, #constructor_args)*) -> Self {
+                Self { client: ::combadge::prelude::Client::new(worker) }
+            }
+        }
+    };
+
+    let server_name = format_ident!("{}Server", item.ident);
+    let server = quote! {
+        pub struct #server_name {
+            server: std::rc::Rc<std::cell::RefCell<::combadge::prelude::Server>>,
+        }
+
+        impl #server_name {
+            pub fn new() -> Self {
+                log::info!("inside ss new");
+                Self {
+                    server: Server::new(),
+                }
+            }
+        }
+    };
+
+    let result: TokenStream = quote! {
+        #item
+        #client
+        #server
+    }
+    .into();
+
+    // println!("{}", prettyplease::unparse(&parse(result.clone()).unwrap()));
+
+    result
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,75 @@
 extern crate combadge_macros;
-pub use combadge_macros::combadge;
+
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+use wasm_bindgen::prelude::*;
+use web_sys::js_sys::global;
+use web_sys::{DedicatedWorkerGlobalScope, MessageEvent, Worker};
+
+pub mod prelude {
+    pub use ::web_sys;
+    pub use combadge_macros::combadge;
+
+    pub use crate::{Client, Server};
+}
+
+#[derive(Debug)]
+pub struct Client {
+    weak_self: Weak<RefCell<Self>>,
+    on_message: Closure<dyn Fn(MessageEvent)>,
+    worker: Worker,
+}
+
+impl Client {
+    pub fn new(worker: Worker) -> Rc<RefCell<Self>> {
+        let client = Rc::new_cyclic(|weak_self| {
+            let on_message = Closure::new(|event: MessageEvent| {
+                #[cfg(feature = "log")]
+                log::info!("Client received message: {event:?}");
+            });
+
+            #[cfg(feature = "log")]
+            log::info!("Setting onmessage on client");
+            worker.set_onmessage(Some(on_message.as_ref().unchecked_ref()));
+
+            #[cfg(feature = "log")]
+            log::info!("Sending hello to server");
+            worker.post_message(&JsValue::from_str("hello from client"));
+
+            RefCell::new(Self {
+                weak_self: weak_self.clone(),
+                on_message,
+                worker,
+            })
+        });
+
+        client
+    }
+}
+
+pub struct Server {
+    on_message: Closure<dyn Fn(MessageEvent)>,
+}
+
+impl Server {
+    pub fn new() -> Rc<RefCell<Self>> {
+        let global: JsValue = global().into();
+        let scope: DedicatedWorkerGlobalScope = global.into();
+
+        let on_message = Closure::new(|event: MessageEvent| {
+            #[cfg(feature = "log")]
+            log::info!("Server received message: {event:?}");
+        });
+
+        #[cfg(feature = "log")]
+        log::info!("Setting onmessage on server");
+        scope.set_onmessage(Some(on_message.as_ref().unchecked_ref()));
+
+        #[cfg(feature = "log")]
+        log::info!("Sending hello to client");
+        scope.post_message(&JsValue::from_str("hello from server"));
+
+        Rc::new(RefCell::new(Self { on_message }))
+    }
+}
