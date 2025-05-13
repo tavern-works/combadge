9b8f0a723f18118921b6af6e3fb7826f798db76b
Dan Stoza <stoza@periapt.media>
2025-05-13 16:58:07 -0700

    Make base variable for GH deploy

--- sample/vite.config.ts
+++ sample/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
+    base: "",
     plugins: [wasm()],
     worker: {
         format: "es",
